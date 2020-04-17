caef5f3dac12c426e2a40d36ea33f066e4e4c0a5
pointerboy <lukatrbovic@outlook.com>
2020-04-17 20:48:20 +0200

    added DownloadConfig + replaced parser

--- ViceCityVR/VRNetwork.cpp
+++ ViceCityVR/VRNetwork.cpp
@@ -1,7 +1,23 @@
 #include "VRNetwork.h"
+#include "INIReader.h"
 
 VRNetwork::VRNetwork()
 {
-	update_url = "https:///www.vanilla-remastered.com/vc.config";
+	update_url = "https:///www.vanilla-remastered.com/files/vc.config";
+}
+
+void VRNetwork::DownloadConfig()
+{
+	HRESULT downloadResult = URLDownloadToFileA(NULL, update_url,
+		"vc.config", 0, NULL);
+
+	if (SUCCEEDED(downloadResult))
+	{
+		ParseConfig();
+	}
+}
+
+void VRNetwork::ParseConfig()
+{
 }
 
--- ViceCityVR/VRNetwork.h
+++ ViceCityVR/VRNetwork.h
@@ -1,10 +1,14 @@
+
 #pragma once
+#include <Windows.h>
+#include <fstream>
 class VRNetwork
 {
 public:
 	VRNetwork();
 
 	void DownloadConfig();
+	void ParseConfig();
 private:
 	static const char* update_url;
 };
