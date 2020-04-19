34c1c52e909522c4ef1097367e2056e020d67c62
pointerboy <lukatrbovic@outlook.com>
2020-04-19 23:52:28 +0200

    added cpprestdsk

--- ViceCityVR/ViceCityVR.cpp
+++ ViceCityVR/ViceCityVR.cpp
@@ -1,21 +1,28 @@
-#include <plugin.h>
-#include "RenderWare.h"
-#include "d3dx9.h"
-#include "rwd3d9.h"
+#include "plugin.h"
+#include "CMessages.h"
+#include "CModelInfo.h"
+#include "extensions/ScriptCommands.h"
 
 #include <windows.h>
-#include <thread>
+#include <Lmcons.h>
 
 using namespace plugin;
 
-class ViceCityVR {
+class ScriptCommands {
 public:
-    ViceCityVR() {
-        SetWindowText(GetActiveWindow(), "Vanilla Remastered Vice City");
-        HICON ico = (HICON)LoadImage(NULL, "favi.ico", IMAGE_ICON, 32, 32, LR_LOADFROMFILE);
-        SendMessage(GetActiveWindow(), WM_SETICON, ICON_SMALL, (LPARAM)ico);
+    ScriptCommands() {
+        Events::processScriptsEvent += [] {
+            char username[UNLEN + 1];
+            DWORD username_len = UNLEN + 1;
+            GetUserName((LPWSTR)username, &username_len);
+
+
+            if (Command<Commands::IS_PLAYER_PLAYING>(0))
+            {
+                    static char message[400];
+                    snprintf(message, 400, "www.vanilla-remastered - id:%s", username);
+                    CMessages::AddMessageJumpQ(message, 500, 1);
+            }
+        };
     }
-
-    ~ViceCityVR() {}
-} viceCityVR;
-
+} scriptCommands;
