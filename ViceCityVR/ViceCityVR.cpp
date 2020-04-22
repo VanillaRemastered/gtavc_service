166301c2bcfcd908d372c7395501dc937721d5ce
pointerboy <lukatrbovic@outlook.com>
2020-04-23 01:22:28 +0200

    removed cpprest

--- ViceCityVR/ViceCityVR.cpp
+++ ViceCityVR/ViceCityVR.cpp
@@ -3,26 +3,19 @@
 #include "CModelInfo.h"
 #include "extensions/ScriptCommands.h"
 
-#include <windows.h>
-#include <Lmcons.h>
+#include <CHud.h>
 
 using namespace plugin;
 
 class ScriptCommands {
 public:
     ScriptCommands() {
+        Events::gameProcessEvent += [] {
+           
+            
+        };
         Events::processScriptsEvent += [] {
-            char username[UNLEN + 1];
-            DWORD username_len = UNLEN + 1;
-            GetUserName((LPWSTR)username, &username_len);
 
-
-            if (Command<Commands::IS_PLAYER_PLAYING>(0))
-            {
-                    static char message[400];
-                    snprintf(message, 400, "www.vanilla-remastered - id:%s", username);
-                    CMessages::AddMessageJumpQ(message, 500, 1);
-            }
         };
     }
 } scriptCommands;
