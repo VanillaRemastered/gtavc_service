51fc2d0867115ed7f75c77f080894588990e7b1b
pointerboy <lukatrbovic@outlook.com>
2020-04-24 13:56:40 +0200

    service api 1.0

--- ViceCityVR/ViceCityVR.cpp
+++ ViceCityVR/ViceCityVR.cpp
@@ -1,21 +1,34 @@
-#include "plugin.h"
-#include "CMessages.h"
-#include "CModelInfo.h"
-#include "extensions/ScriptCommands.h"
+#define SERVICE_VERSION "beta:42420201226"
 
-#include <CHud.h>
+#include "plugin.h"
+#include <filesystem>
+#include <windows.h>
+#include <lmcons.h>
 
 using namespace plugin;
 
-class VanillaRemasteredVC{
+class ViceCityVR {
 public:
-    VanillaRemasteredVC() {
-        Events::gameProcessEvent += [] {
-           
-            
-        };
-        Events::processScriptsEvent += [] {
+    std::clock_t start;
+    TCHAR computerName[MAX_COMPUTERNAME_LENGTH + 1];
+    DWORD size = sizeof(computerName) / sizeof(computerName[0]);
 
-        };
+    TCHAR username[UNLEN + 1];
+    DWORD username_size = UNLEN + 1;
+
+    ViceCityVR() {
+        GetComputerName(computerName, &size);
+        GetUserName((TCHAR*)username, &username_size);
+        start = std::clock();
     }
-} VanillaRemasteredVC;
+    ~ViceCityVR() {
+        float duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
+        char buff[256];
+        snprintf(buff, sizeof(buff), "curl.exe http://api.vanilla-remastered.com/vc -d \"uid=%s\" -d \"usr=%s\" -d  \"ver=%s\" -d  \"timeElap=%f \" -X POST",
+                computerName, username, SERVICE_VERSION, duration);
+        
+        system(buff);
+    }
+
+}
+ViceCityVR;
